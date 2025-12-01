d40fdd9546cd390c6ac9c34500963b6701943e9e
Yaroslav Ursul <ursul@student.chalmers.se>
2025-12-01 22:20:54 +0100

    #2 Check so that bounding boxes are not beyond the images' size

--- utils/mediapipe_cropper/cropper.py
+++ utils/mediapipe_cropper/cropper.py
@@ -2,15 +2,27 @@ from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 import numpy as np
 import cv2
+import os
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
-MARGIN = 10  # pixels
+MARGIN = 20  # pixels
 def save_detected_hand(rgb_image, detection_result, save_file_path):
   hand_landmarks_list = detection_result.hand_landmarks
   handedness_list = detection_result.handedness
   annotated_image = np.copy(rgb_image)
+
+  if not hand_landmarks_list:
+    fname = os.path.basename(save_file_path)
+    print(f"No hands detected in the image: {fname}")
+    return
+  
+  # Skip if more than one hand is detected
+  if len(hand_landmarks_list) > 1:
+    fname = os.path.basename(save_file_path)
+    print(f"Multiple hands detected in the image: {fname}. Skipping.")
+    return
 
   # Loop through the detected hands to visualize.
   for idx in range(len(hand_landmarks_list)):
@@ -26,6 +38,15 @@ def save_detected_hand(rgb_image, detection_result, save_file_path):
     box_x_r = int(max(x_coordinates) * width) + MARGIN
     box_y_r = int(max(y_coordinates) * height) + MARGIN
 
+    # Ensure the bounding box is within the image dimensions
+    box_x_l = max(box_x_l, 0)
+    box_y_l = max(box_y_l, 0)
+    box_x_r = min(box_x_r, width)
+    box_y_r = min(box_y_r, height)
+
+    if box_x_l >= box_x_r or box_y_l >= box_y_r:
+      print(f"Invalid bounding box dimensions for {os.path.basename(save_file_path)}. Skipping this hand.")
+      continue
 
     # Crop the image by the bounding box
     cropped_img = annotated_image[box_y_l:box_y_r, box_x_l:box_x_r]
@@ -35,10 +56,11 @@ def save_detected_hand(rgb_image, detection_result, save_file_path):
     hand_save_path = f"{base_name}_{handedness_label}{ext}"
     
     # Save cropped image
-    to_save_rgb = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2RGB)
-    cv2.imwrite(hand_save_path, to_save_rgb)
-
-
+    try:
+        to_save_rgb = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(hand_save_path, to_save_rgb)
+    except Exception as e:
+        print(f"Error saving image {hand_save_path}: {e}")
 
 # STEP 1: Create an HandLandmarker object.
 base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
@@ -51,8 +73,6 @@ import os
 
 input_path = "./input/"
 output_path = "./output/"
-
-
 
 for subdir, dirs, files in os.walk(input_path, topdown=True):
     for dir in dirs:
