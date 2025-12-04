bf9256b08cf7829945df0ed40b37afbc10f50dca
Maksym Matsuhyria <maksymm@student.chalmers.se>
2025-12-04 18:46:31 +0100

    #2 make cropper runnable as module

--- utils/mediapipe_cropper/cropper.py
+++ utils/mediapipe_cropper/cropper.py
@@ -4,17 +4,53 @@ import os
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
-import os
 import argparse
 import tqdm
 
 MARGIN = 20  # pixels
 
+def main():
+    args = parse_args()
+    process_images(args.input, args.output)
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-input", "--input", dest = "input", default = "input/", help="Path to input folder")
-    parser.add_argument("-output", "--output", dest = "output", default = "output/", help="Path to output folder")
+    parser.add_argument("-i", "--input", dest = "input", default = "input/", help="Path to input folder")
+    parser.add_argument("-o", "--output", dest = "output", default = "output/", help="Path to output folder")
     return parser.parse_args()
+
+def process_images(input_path, output_path):
+    # STEP 0: Resolve detector path.
+    detector_path = os.path.join(os.path.dirname(__file__), 'hand_landmarker.task')
+
+    # STEP 1: Create an HandLandmarker object.
+    base_options = python.BaseOptions(model_asset_path=detector_path)
+    options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
+    detector = vision.HandLandmarker.create_from_options(options)
+
+    # STEP 2: Iterate through images in the input folder.
+    labels = [l for l in os.listdir(input_path) if os.path.isdir(os.path.join(input_path, l))]
+
+    for label in labels:
+        label_input_path = os.path.join(input_path, label)
+        label_output_path = os.path.join(output_path, label)
+
+        os.makedirs(label_output_path, exist_ok=True) # make sure dir exists
+
+        tqdm.tqdm.write(f"Processing directory: {label_input_path}")
+
+        files = [f for f in os.listdir(label_input_path) if f.endswith(('.png', '.jpg', '.jpeg'))]
+
+        for file in tqdm.tqdm(files, unit="img"):
+    # STEP 3: Load the input image.
+            image = mp.Image.create_from_file(os.path.join(label_input_path, file))
+
+    # STEP 4: Detect hand landmarks from the input image.
+            detection_result = detector.detect(image)
+
+    # STEP 5: Save cropped image
+            # slice to get rid of alpha channel if present
+            save_detected_hand(image.numpy_view()[:,:,:3], detection_result, os.path.join(label_output_path, file))
 
 def save_detected_hand(rgb_image, detection_result, save_file_path):
     hand_landmarks_list = detection_result.hand_landmarks
@@ -22,16 +58,15 @@ def save_detected_hand(rgb_image, detection_result, save_file_path):
 
     if not hand_landmarks_list:
         fname = os.path.basename(save_file_path)
-        print(f"No hands detected in the image: {fname}")
+        print(f"No hands detected in the image: {fname}. Skipping.")
         return
 
-    # Skip if more than one hand is detected
     if len(hand_landmarks_list) > 1:
         fname = os.path.basename(save_file_path)
         print(f"Multiple hands detected in the image: {fname}. Skipping.")
         return
 
-# Loop through the detected hands to visualize.
+    # Extract the first and only detected hand
     hand_landmarks = hand_landmarks_list[0]
 
     # Get the detected hand's bounding box.
@@ -60,36 +95,9 @@ def save_detected_hand(rgb_image, detection_result, save_file_path):
         print("Empty crop, skipping...")
         return
 
-    if not os.path.exists(os.path.dirname(save_file_path)):
-        os.makedirs(os.path.dirname(save_file_path))
-    
     # Save cropped image
     to_save_rgb = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2RGB)
     cv2.imwrite(save_file_path, to_save_rgb)
 
-args = parse_args()
-input_path = args.input
-output_path = args.output
-
-print(f"Input path: {input_path}")
-print(f"Output path: {output_path}")
-
-# STEP 1: Create an HandLandmarker object.
-base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
-options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
-detector = vision.HandLandmarker.create_from_options(options)
-
-for subdir, dirs, files in os.walk(input_path, topdown=True):
-    print(f"Processing directory: {subdir}")
-    for file in tqdm.tqdm(files, unit="img"):
-        if not file.endswith((".jpg", ".jpeg", ".png")):
-            continue
-        file = os.path.relpath(os.path.join(subdir, file), input_path)
-# STEP 2: Load the input image.
-        image = mp.Image.create_from_file(os.path.join(input_path, file))
-
-# STEP 3: Detect hand landmarks from the input image.
-        detection_result = detector.detect(image)
-
-# STEP 4: Save cropped image 
-        save_detected_hand(image.numpy_view()[:,:,:3], detection_result, os.path.join(output_path, file))
+if __name__ == "__main__":
+    main()
