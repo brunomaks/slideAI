61e3c2c44f749b793c796fdd44f9b6847ab15071
Ahmet Yavuz Kalkan <ahmety@student.chalmers.se>
2025-12-07 20:21:22 +0100

    #17 Update flip app to resize instead

--- web_app/apps/flip/views.py
+++ web_app/apps/flip/views.py
@@ -6,26 +6,51 @@ import cv2
 import numpy as np
 import os
 import time
+import tqdm
 
 #csrf_exempt is needed since without it we get a 403 error when posting the request
 @csrf_exempt
 def flip_view(request):
+    print("Flip service received a request")
+    
     if request.method != "POST":
         return JsonResponse({"error": "POST an image file"}, status=400)
-    print("Flip service received a request")
+    
+    size = int(request.headers.get('X-Resize-Size', 94))
+    
+    # STEP 1: Load the input image. 
     nparr = np.frombuffer(request.body, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    # flip the image horizontally
-    flipped = cv2.flip(img, 1)
-
-    # save the flipped image to the output folder if header is there
+    # STEP 2: Resize and save the image
+    resized = resize_and_save(img, size)
+    
+    # save the resized image to the output folder if header is there
     if request.headers.get('X-Debug-Save'):
-        filename = "frame" + "_flip_" + str(int(time.time_ns())) + ".jpg"
+        filename = "frame" + "_resize_" + str(int(time.time_ns())) + ".jpg"
         path = os.path.join(settings.MEDIA_ROOT, filename)
-        print("Saving flipped image to disk for debugging at path:", path)
-        cv2.imwrite(path, flipped)
+        print("Saving resized image to disk for debugging at path:", path)
+        cv2.imwrite(path, resized)
 
     # return the flipped image
-    _, buffer = cv2.imencode('.jpg', flipped)
+    _, buffer = cv2.imencode('.jpg', resized)
     return HttpResponse(buffer.tobytes(), content_type='image/jpeg')
+
+def resize_and_save(img, size):
+    h, w = img.shape[:2]
+    scale = size / max(h, w)
+    new_w, new_h = int(w * scale), int(h * scale)
+    resized = cv2.resize(img, (new_w, new_h))
+
+    # compute padding
+    top = (size - new_h) // 2
+    bottom = size - new_h - top
+    left = (size - new_w) // 2
+    right = size - new_w - left
+
+    padded = cv2.copyMakeBorder(
+        resized, top, bottom, left, right,
+        cv2.BORDER_CONSTANT, value=[0,0,0]
+    )
+
+    return padded
