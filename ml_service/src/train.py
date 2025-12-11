6ba524d295fcf822b9b7a4fc0b87a6f3f5790ce3
Maksym <maksymm@student.chalmers.se>
2025-12-11 21:52:21 +0100

    #27 add precision/recall during training

--- ml_service/src/train.py
+++ ml_service/src/train.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
+from tensorflow.keras import Precision, Recall, Accuracy
 import argparse
 from pathlib import Path
 from datetime import datetime
@@ -88,7 +89,9 @@ def train_model(args):
     model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
         loss='sparse_categorical_crossentropy',
-        metrics=['accuracy']
+        metrics=['accuracy',
+                 Precision('precision'),
+                 Recall('recall')]
     )
 
     model.summary()
