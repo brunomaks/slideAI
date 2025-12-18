e7735a8d97084d0db75c45a07c0344f2d0749b1d
Maksym <maksymm@student.chalmers.se>
2025-12-18 23:43:20 +0100

    #32 style the popup

--- frontend/src/components/ExitPopup.jsx
+++ frontend/src/components/ExitPopup.jsx
@@ -1,15 +1,17 @@
-import { createPortal } from "react-dom";
+// We don't want to render the popup inside Reveal or PDF containers. 
+// Portal allows to render it into document.body
+import { createPortal } from "react-dom"; // we 
 import './ExitPopup.css';
 
-const ExitPopup = ({ onConfirm, onCancel }) => {
+const ExitPopup = () => {
     return createPortal(
         <div className="popup-overlay">
             <div className="popup">
-                <p>Do you want to exit the slide preview?</p>
+                <p className="popup-title">Do you want to exit the slide preview?</p>
 
-                <p>Perform "like" to exit</p>
+                <p className="popup-instruction">Perform <strong>"like"</strong> to exit</p>
 
-                <p>Perform "stop" to close this window</p>
+                <p className="popup-instruction">Perform <strong>"stop"</strong> to close this window</p>
             </div>
         </div>,
         document.body
@@ -17,4 +19,3 @@ const ExitPopup = ({ onConfirm, onCancel }) => {
 };
 
 export default ExitPopup;
-
