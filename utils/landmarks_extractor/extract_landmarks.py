2b3b8abebb3330ba535d65237bc9270491873006
Maksym Matsuhyria <maksymm@student.chalmers.se>
2025-12-22 13:49:17 +0100

    add dataset augmentation

--- utils/landmarks_extractor/extract_landmarks.py
+++ utils/landmarks_extractor/extract_landmarks.py
@@ -1,3 +1,4 @@
+import random
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
@@ -22,40 +23,28 @@ options = HandLandmarkerOptions(base_options=BaseOptions(model_asset_path=str(DE
                                 num_hands=1,
                                 running_mode=VisionRunningMode.IMAGE)
 
+class DatasetOptions:
+    def __init__(self, augment=False, augment_gestures=None, augment_count=0, augment_angles=None):
+        self.augment = augment
+        self.augment_gestures = augment_gestures if augment_gestures else []
+        self.augment_count = augment_count
+        self.augment_angles = augment_angles if augment_angles else []
+
+dataset_options = DatasetOptions(
+    augment=True,
+    augment_gestures=['two_up_inverted'],
+    augment_count=2,
+    augment_angles=[-30, -45, -60, -90]
+)
+
 
 def main():
     # args = parse_args()
     # process_image(args.input, args.output)
-    # test_image = '/home/max/projects/slide-ai/shared_artifacts/images/hagrid_30k/0aa54e32-7418-484b-b855-9d6c9d04a46c.jpg'
-    # process_image(test_image, "call")
     input_path = BASE_IMAGE_DIR
-
-    dataset = []
-    skipped_count = 0
-
-    with HandLandmarker.create_from_options(options) as landmarker:
-        for gesture_folder in os.listdir(input_path):
-            gesture_path = input_path / gesture_folder
-            for file in tqdm.tqdm(os.listdir(gesture_path), desc=f"Processing {gesture_folder}"):
-                image_path = gesture_path / file
-                result = process_image(image_path, gesture_folder, landmarker)
-                if result:
-                    dataset.append(result)
-                else:
-                    skipped_count += 1
-
     output_path = BASE_IMAGE_DIR / "hagrid_30k_landmarks.json"
-
-    print(f"Saving JSON to: {output_path}")
-    print(f"Parent directory exists? {output_path.parent.exists()}")
-
-    try:
-        output_path.parent.mkdir(parents=True, exist_ok=True)
-        with open(output_path, "w") as f:
-            json.dump(dataset, f, indent=2)
-        print(f"File saved successfully.")
-    except Exception as e:
-        print(f"Error writing file: {e}")
+    dataset = build_dataset(input_path, dataset_options)
+    save_dataset(dataset, output_path)
 
 
 def parse_args():
@@ -64,21 +53,62 @@ def parse_args():
     parser.add_argument("-o", "--output", dest = "output", default = "output/landmarks_raw.json", help="Path to output .json file")
     return parser.parse_args()
 
-def process_image(image_path, gesture_name, landmarker):
-    image_Path = Path(image_path)
+def build_dataset(input_path, dataset_options):
+    dataset = []
+    skipped_count = 0
 
-    image = cv2.imread(image_path)
+    with HandLandmarker.create_from_options(options) as landmarker:
+        for gesture_folder in os.listdir(input_path):
+            gesture_path = input_path / gesture_folder
+            for file in tqdm.tqdm(os.listdir(gesture_path), desc=f"Processing {gesture_folder}"):
+                image_path = gesture_path / file
+                landmarks = extract_landmarks(image_path, landmarker)
+                result = process_landmarks(landmarks, gesture_folder, image_path)
+                if result:
+                    dataset.append(result)
+                else:
+                    skipped_count += 1
+
+                if dataset_options.augment:
+                    if gesture_folder in dataset_options.augment_gestures:
+                        for _ in range(dataset_options.augment_count):
+                            angle = random.choice(dataset_options.augment_angles)
+                            result = process_landmarks(landmarks, gesture_folder, image_path, angle)
+                            if result:
+                                dataset.append(result)
+                            else:
+                                skipped_count += 1 
+    
+    print(f"Total images processed(and augmented?): {len(dataset) + skipped_count}")
+    print(f"Total images skipped (no hand detected): {skipped_count}")
+    return dataset
+
+def save_dataset(dataset, output_path):
+    try:
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(output_path, "w") as f:
+            json.dump(dataset, f, indent=2)
+        print(f"File saved successfully.")
+    except Exception as e:
+        print(f"Error writing file: {e}")
+
+def extract_landmarks(image_path, landmarker):
+    image = cv2.imread(str(image_path))
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image_rgb)
 
     results = landmarker.detect(mp_image)
 
-    if not results.hand_landmarks:
+    return results
+
+
+def process_landmarks(input, gesture_name, image_path, rotate_angle_deg=0):
+    if not input.hand_landmarks:
         return
 
-    landmarks = results.hand_landmarks[0]
-    handedness_category = results.handedness[0][0]
+    landmarks = input.hand_landmarks[0]
+    handedness_category = input.handedness[0][0]
     handedness = handedness_category.category_name
     confidence = handedness_category.score
 
@@ -86,8 +116,10 @@ def process_image(image_path, gesture_name, landmarker):
     for lm in landmarks:
         landmark_list.append([lm.x, lm.y])
 
-    normalized_landmarks = normalize_landmarks(landmark_list, handedness)
+    normalized_landmarks = normalize_landmarks(landmark_list, handedness, rotate_angle_deg)
     
+    # using pathlib for relative paths 
+    image_Path = Path(image_path)
     return {
         "gesture": gesture_name,
         "handedness": handedness,
@@ -96,7 +128,7 @@ def process_image(image_path, gesture_name, landmarker):
         "image_path": str(image_Path.relative_to(BASE_IMAGE_DIR))
     }
 
-def normalize_landmarks(landmarks, handedness):
+def normalize_landmarks(landmarks, handedness, rotate_angle_deg=0):
     landmarks = np.array(landmarks)
 
     # Translate so that wrist is at origin
@@ -113,7 +145,31 @@ def normalize_landmarks(landmarks, handedness):
     if handedness == "Left":
         landmarks[:, 0]  =  -landmarks[:, 0]
 
+    if rotate_angle_deg != 0:
+        landmarks = rotate_landmarks(landmarks, rotate_angle_deg)
+
     return landmarks.tolist()
+
+# to be able to detect rotated hands
+def rotate_landmarks(landmarks, angle_deg):
+    angle = np.deg2rad(angle_deg)
+
+    # 2d rotation matrix
+    R = np.array([
+        [np.cos(angle), -np.sin(angle)],
+        [np.sin(angle),  np.cos(angle)]
+    ]) # 2,2
+
+    center = landmarks.mean(axis=0)
+
+    # make center to be the origin
+    landmarks = landmarks - center # 21,2
+
+    # perform rotation and translate back
+    landmarks = (landmarks @ R.T) + center
+
+    return landmarks
+
 
 if __name__ == "__main__":
     main()
