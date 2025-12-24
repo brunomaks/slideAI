41434f045e6cf8aa427a1a0c44525f9b0007263d
Pavlo Ovchynnykov <pavlo.ovchynnykov238@gmail.com>
2025-12-24 10:45:58 +0100

    add tensorflow to pytorch model converter

---
+++ utils/model_converter/model_converter.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import torch
+import tf2onnx
+import onnx 
+from onnx2pytorch import ConvertModel 
+
+MODEL_PATH = "../../shared_artifacts/models/gesture_model_20251221_184630.keras"
+
+# Load tensorflow model 
+tf_model = tf.keras.models.load_model(MODEL_PATH)
+tf_model.output_names=['output']
+
+
+# Convert model to onnx format
+onnx_model, _ = tf2onnx.convert.from_keras(tf_model)
+
+# Convert ONNX model to PyTorch 
+pytorch_model = ConvertModel(onnx_model) 
+
+torch.save(pytorch_model, "pytorch_model_full.pth")
