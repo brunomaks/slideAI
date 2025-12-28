30a3c987bb00ab8656f1f7abd6333baebf6e5b48
Mahmoud Khalafallah <mahmoudk@chalmers.se>
2025-12-28 02:48:51 +0100

    remove ImageMetadata model

--- web_app/apps/core/models.py
+++ web_app/apps/core/models.py
@@ -10,30 +10,6 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class ImageMetadata(BaseModel):
-    """Store metadata for training images."""
-    filename = models.CharField(max_length=255)
-    label = models.CharField(max_length=50, db_index=True)
-    width = models.IntegerField()
-    height = models.IntegerField()
-    file_path = models.CharField(max_length=512)
-    dataset_type = models.CharField(
-        max_length=10, 
-        choices=[('train', 'Training'), ('test', 'Test'), ('validation', 'Validation')],
-        default='train',
-        db_index=True
-    )
-    is_synthetic = models.BooleanField(default=False)
-    source_dataset = models.CharField(max_length=100, default='hagrid')
-
-    class Meta:
-        db_table = 'image_metadata'
-        indexes = [
-            models.Index(fields=['label', 'dataset_type']),
-        ]
-
-    def __str__(self):
-        return f"{self.filename} ({self.label})"
 
 
 class ModelVersion(BaseModel):
